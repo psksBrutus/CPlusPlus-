9b70e28be5deca9a95e6e0e322b78c9b483adf51
Marcel Radischat <sabodendar@googlemail.com>
2011-06-01 18:14:36 +0200

    added files

---
+++ osmconvert/osmconvert/ConvertController.cpp
@@ -0,0 +1,9 @@
+//
+//  ConvertController.cpp
+//  OSM_Konverter
+//
+//  Created by Marcel Radischat on 01.06.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include "ConvertController.h"
